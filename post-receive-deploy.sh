71cd79a6f1ad273b017554b83aa1bd08cac7b095
Damien Radtke <git@damienradtke.com>
2020-06-18 12:38:31 -0500

    Update post-receive deploy hook

--- post-receive-deploy.sh
+++ post-receive-deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-echo "hello from post-receive! here is what i see:"
-cat -
-exit 0
+REF_TO_DEPLOY="refs/heads/master"
+JOB_PATH="/home/damien/infrastructure/jobs/damienradtkecom.nomad.erb"
+
+while IFS=$'\n' read -r line; do
+        args=(${line})
+        if [[ "${args[2]}" = "${REF_TO_DEPLOY}" ]]; then
+                export REF="${args[1]}"
+                echo "Deploying ${REF}"
+                nomad-compile "${JOB_PATH}" | nomad job run -
+        fi
+done
